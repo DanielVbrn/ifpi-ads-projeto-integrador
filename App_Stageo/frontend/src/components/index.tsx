8a9c0f8490462e9e2465a71355b2d712bd25ab2c
DanielVbrn <you@example.com>
2023-07-10 12:24:58 -0300

    update repository with package.json file

--- App_Stageo/frontend/src/components/index.tsx
+++ App_Stageo/frontend/src/components/index.tsx
@@ -3,27 +3,27 @@ import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./index.css";
 
-interface HeaderProps {
-  isLoggedIn: boolean; // Indica se o usuário está logado ou não
-}
+// interface HeaderProps {
+//   isLoggedIn: boolean; // Indica se o usuário está logado ou não
+// }
 
-const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
+const Header: React.FC/* <HeaderProps> */ = (/* { isLoggedIn } */) => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Navbar.Brand href="#home">STAGEO</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          {isLoggedIn && (
+          {/* {isLoggedIn && ( */}
             <Nav.Item as={Link} className="nav-link" to="/Home">
               Inicio
             </Nav.Item>
-          )}
-          {isLoggedIn && (
+          {/* )} */}
+          {/* {isLoggedIn && ( */}
             <Nav.Link as={Link} className="nav-link" to="/Equipments">
               Equipamentos
             </Nav.Link>
-          )}
+          {/* )} */}
         </Nav>
         <Nav>
           <Nav.Link as={Link} className="bt-logout" to="/">
